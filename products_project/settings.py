c6631a2a183ab9916c13d718ba93a5297c9df7c0
bdavis83 <bdavis83@gmail.com>
2023-01-05 16:28:52 -0500

    Created database and made first migration

--- products_project/settings.py
+++ products_project/settings.py
@@ -73,7 +73,6 @@ WSGI_APPLICATION = 'products_project.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
 
@@ -102,7 +101,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
