a623f566387d9eaddbf703994ec042cf913c4c2c
bdavis83 <bdavis83@gmail.com>
2023-01-06 11:34:48 -0500

    Created serializer.py, views.py and urls.py in products app

--- products/views.py
+++ products/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import get_list_or_404
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from products.serializers import ProductSerializer
+from .models import Product
 
-# Create your views here.
+@api_view(['GET'])
+def product_list (request):
+
+
+    return Response ('ok')
+
+
+
+
+
